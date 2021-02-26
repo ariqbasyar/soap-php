f6046a2c4c68e0ec59e280449420c36f3fb96063
ariqbasyar <ariqbasyar2@gmail.com>
2021-02-26 22:51:19 +0700

    copied from https://gist.github.com/umidjons/5907857 for generated wsdl with php

---
+++ soap/generated-wsdl/soap-client.php
@@ -0,0 +1,24 @@
+<?
+    // https://gist.github.com/umidjons/5907857#file-soap-client-php
+    $client = new SoapClient( "http://localhost/wsdls/organization.wsdl", array( 'cache_wsdl' => WSDL_CACHE_NONE ) );
+    try {
+        $responseLogin = $client->login( 'test_user', 'test_password' ); // call login() from .wsdl
+        if($responseLogin == "success") { // if success
+            $response = $client->doFilter( $params ); // call doFilter() from .wsdl
+            ?>
+            <pre><? print_r( $response ); ?></pre>
+            <?
+        }
+    } catch ( SoapFault $sf ) {
+        echo $sf->getMessage();
+
+        // Full SoapFault message
+        //  echo '<pre>';
+        //  var_dump( $sf );
+        //  echo '</pre>';
+
+        // Analyze last request
+        //  $xml = $client->__getLastRequest();
+        //  echo $xml;
+    }
+?>
